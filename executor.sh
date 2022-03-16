dd5792fb44f60ca66114591c6f6e8e806bda3570
Nikita <nikisazikov56@gmail.com>
2022-03-16 23:53:59 +0300

    Added bash script to open project. It doesnt  work, if you have a password on your linux system

---
+++ executor.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#cd build
+mkdir build
+cd build
+
+cmake ..
+kefile
+
+read readable
+
+change="set(VAR 3)"
+
+# shellcheck disable=SC1068
+writes = "set(VAR $readable)"
+
+sed -i "s/$change/$writes" CMakeLists.txt
+
+cmake --build .
+
+cd /home/nikita/CLionProjects/untitled2/bigInt/build/src/Dynamic
+./libbigInt2.a
